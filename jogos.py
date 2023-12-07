ab9dc0fefad641b9647e7e2b2010365ab11c8830
eaijessicao <jessicasdeoliveira1921@gmail.com>
2023-12-07 10:50:11 -0300

    subindo jogo da forca

--- jogos.py
+++ jogos.py
@@ -1,26 +1,21 @@
 import forca
 import adivinhacao
 
-print("*********************************")
-print("*******Escolha o teu jogo********")
-print("*********************************")
+def escolhe_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
 
+    print("(1) Forca (2) Adivinhação")
 
+    jogo = int(input("Qual jogo? "))
 
-print("(1) forca (2) adivinhação")
+    if(jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
 
-jogo = int(input("Escolha o jogo "))
-
-if(jogo == 1) :
-    print("Você escolheu o jogo da forca. ")
-    print("Iniciando...") 
-    forca.jogar()    
-
-elif(jogo == 2) :
-    print("Você escolheu o jogo da Adivinhação ")
-    print("Iniciando...")  
-    adivinhacao.jogar()
-
-
-
-print("Fim do jogo")
+if(__name__ == "__main__"):
+    escolhe_jogo()
